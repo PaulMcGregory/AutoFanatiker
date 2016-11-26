c41611e6dacfc84b9ffa1b730e444712d4cdbccb
McGregory <info@abiks.de>
2016-11-26 03:43:15 +0100

    First App

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		cordova.InAppBrowser.open('http://www.autofanatiker.de', '_blank', 'location=no,fullscreen=yes');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
